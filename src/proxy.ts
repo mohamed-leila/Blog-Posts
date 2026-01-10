99ade7e289fbed0cc4e53c7a23cfb604565cae59
mohamed-leila <mohamedleila910@gmail.com>
2026-01-10 15:23:40 +0300

    try

--- src/proxy.ts
+++ src/proxy.ts
@@ -7,6 +7,7 @@ export async function proxy(request: NextRequest) {
   // THIS IS NOT SECURE!
   // This is the recommended approach to optimistically redirect users
   // We recommend handling auth checks in each page/route
+  //
   if (!sessionCookie) {
     return NextResponse.redirect(new URL("/auth/login", request.url));
   }
